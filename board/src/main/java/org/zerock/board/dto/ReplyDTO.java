1f4fbf9822b9f84e024b216c6aa4881daf7db041
Administrator <Administrator@304-22>
2022-11-25 13:09:25 +0900

    Merge branch 'master' of https://github.com/klssh3126/ex_spring.git

--- board/src/main/java/org/zerock/board/dto/ReplyDTO.java
+++ board/src/main/java/org/zerock/board/dto/ReplyDTO.java
@@ -1,5 +1,26 @@
 package org.zerock.board.dto;
 
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ReplyDTO {
 
+	private Long rno;
+	
+	private String text;
+	
+	private String replyer;
+	
+	private Long bno; //게시글번호
+	
+	private LocalDateTime regDate, modDate;
+	
 }

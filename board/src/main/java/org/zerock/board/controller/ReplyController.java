1ccb72d02451c8ccb75c22b334ee3c25fb98175c
Administrator <Administrator@304-22>
2022-11-28 16:50:13 +0900

    댓글 삭제 기능 구현완료

--- board/src/main/java/org/zerock/board/controller/ReplyController.java
+++ board/src/main/java/org/zerock/board/controller/ReplyController.java
@@ -6,8 +6,11 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.board.dto.ReplyDTO;
@@ -16,7 +19,7 @@ import org.zerock.board.service.ReplyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
-@RequestMapping("/replies")
+@RequestMapping("/replies/")
 @Log4j2
 @RequiredArgsConstructor
 @RestController
@@ -26,7 +29,23 @@ public class ReplyController {
 	@GetMapping(value = "/board/{bno}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<ReplyDTO>> getListByBoard(@PathVariable("bno") Long bno){
 		log.info("bno: " +bno);
-		
 		return new ResponseEntity<>(replyService.getList(bno),HttpStatus.OK);
+	}
+	
+	@PostMapping("")
+	public ResponseEntity<Long> register(@RequestBody ReplyDTO replyDTO){
+		
+		log.info(replyDTO);
+		Long rno = replyService.register(replyDTO);
+		return new ResponseEntity<>(rno,HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{rno}")
+	public ResponseEntity<String> remove(@PathVariable("rno") Long rno){
+		log.info("RNO:" +rno);
+		
+		replyService.remove(rno);
+		
+		return new ResponseEntity<>("success",HttpStatus.OK);
 	}
 }

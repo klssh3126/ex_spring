78d8e5c1a99e4c248261696b0049e3465c9ac870
Administrator <Administrator@304-22>
2022-12-02 17:34:46 +0900

    mreview 플젝 완성

---
+++ mreview/src/main/java/org/zerock/mreview/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package org.zerock.mreview.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.zerock.mreview.entity.Member;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+
+
+	
+}

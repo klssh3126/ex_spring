81a62998e1c83e806e8f45f36f45d665943b53cd
Administrator <Administrator@304-22>
2022-12-12 18:57:47 +0900

    시큐리티 공부

---
+++ club/src/main/java/org/zerock/club/controller/SampleController.java
@@ -0,0 +1,28 @@
+package org.zerock.club.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j2;
+
+@Controller
+@Log4j2
+@RequestMapping("/sample/")
+public class SampleController {
+
+	@GetMapping("/all")
+	public void exAll() {
+		log.info("exAll.......");
+	}
+	
+	@GetMapping("/member")
+	public void exMember() {
+		log.info("exMember......");
+	}
+	
+	@GetMapping("/admin")
+	public void exAdmin() {
+		log.info("exAdmin......");
+	}
+}
